3b6a58ab15cb18ae01e36f16075079ad039aa299
Matthew Denton <mdenton@cs.stanford.edu>
2017-06-02 09:08:24 +0000

    Made it prettier

--- normalTransmission.py
+++ normalTransmission.py
@@ -13,7 +13,25 @@ data = list()
 startSeqNo = 0
 FileName = "normal.npy"
 
+FIN = 0x01
+
 socket = conf.L2socket(iface="client-eth0")
+
+syn = IP(dst=IP_DST) / TCP(window=65535, sport=SRC_PORT, dport=DST_PORT, flags='S')
+IP_SRC = syn[IP].src
+SRC_PORT = syn[TCP].sport
+
+print "sending SYN....."
+syn_ack = sr1(syn)
+initialTs = syn_ack.time
+initialSeq = syn_ack[TCP].seq
+print "Received SYN_ACK!"
+getStr = 'GET / HTTP/1.1\r\n\r\n'
+request = IP(dst=IP_DST) / TCP(window=65535, dport=DST_PORT, sport=SRC_PORT,
+             seq=(syn_ack[TCP].ack), ack=(syn_ack[TCP].seq + 1), flags='A') / getStr
+
+print "Sending Request..."
+socket.send(Ether() / request)
 
 def addACKs(pkt):
   global DST_PORT, IP_DST, startSeqNo, data, socket
@@ -33,29 +51,17 @@ def addACKs(pkt):
   tcp_header_len = pkt.getlayer(TCP).dataofs * 32 / 8
   tcp_seg_len = ip_total_len - ip_header_len - tcp_header_len
   
-  data.append((pkt.time, pkt[TCP].seq - startSeqNo))
+  data.append((pkt.time - initialTs, pkt[TCP].seq - initialSeq))
+  add = 0
+  if pkt.flags & FIN:
+    add = 1
+
   ack_pkt = IP(dst=IP_DST) / TCP(window=65535, dport=DST_PORT, sport=SRC_PORT,
-             seq=(pkt[TCP].ack), ack=(pkt[TCP].seq + tcp_seg_len), flags='A')
+             seq=(pkt[TCP].ack), ack=(pkt[TCP].seq + tcp_seg_len + 1), flags='A')
   socket.send(Ether() / ack_pkt)
 
-syn = IP(dst=IP_DST) / TCP(window=65535, sport=SRC_PORT, dport=DST_PORT, flags='S')
-IP_SRC = syn[IP].src
-SRC_PORT = syn[TCP].sport
-startSeqNo = syn[TCP].seq
-print "sending SYN....."
-syn_ack = sr1(syn)
-print "Received SYN_ACK!"
-getStr = 'GET / HTTP/1.1\r\n\r\n'
-request = IP(dst=IP_DST) / TCP(window=65535, dport=DST_PORT, sport=SRC_PORT,
-             seq=(syn_ack[TCP].ack), ack=(syn_ack[TCP].seq + 1), flags='A') / getStr
-
-#print sr1(request)
-
-print "Sending Request..."
-socket.send(Ether() / request)
-
 print("Sniffing......")
-sniff(iface="client-eth0", prn=addACKs, filter="tcp and ip", timeout=5)
+sniff(iface="client-eth0", prn=addACKs, filter="tcp and ip", timeout=4)
 numbas = np.asarray(zip(*data))
 sp.call(["rm", "-f", FileName], shell=True)
 np.save(FileName, numbas)
