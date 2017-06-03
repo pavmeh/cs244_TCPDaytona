4ffc3debad11699687dbb1ceb364dbc534a79e6d
Matthew Denton <mdenton@cs.stanford.edu>
2017-06-03 02:12:28 +0000

    Generalizing script for lwip and other attacks

--- mininetTopo.py
+++ mininetTopo.py
@@ -10,7 +10,9 @@ from mininet.cli import CLI
 from mininet.node import OVSController
 
 parser = argparse.ArgumentParser(description="Run experiment")
-parser.add_argument('--manual', help='Manual mininet commands', action='store_true')
+parser.add_argument("--client", type=str, choices=["normal", "opACK", "splitACK", "dupACK"], default="normal")
+parser.add_argument("--server", type=str, choices=["linux", "lwip"], default="linux")
+parser.add_argument("--manual", help="Manual mininet commands", action="store_true")
 args = parser.parse_args()
 
 PORT = 8888
@@ -35,12 +37,20 @@ net.pingAll()
 client = net.get('client')
 server = net.get('server')
 
-client.cmd("iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP")
+#client.cmd("iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP")
+
+client.cmd('sysctl net.ipv4.ip_forward=1')
+
+server.cmd('sysctl net.core.netdev_max_backlog=500000')
+server.cmd('sysctl net.ipv4.tcp_congestion_control=cubic')
 
 if args.manual:
   CLI(net)
 else:
-  server.popen("python webserver.py", shell=True)
+  if args.server == "linux":
+    server.popen("python webserver.py", shell=True)
+  elif args.server == "lwip":
+    server.popen("./lwip-contrib/ports/unix/unixsim/simhost", shell=True)
   time.sleep(1)
 
   client.popen("python normalTransmission.py %s %s" % (server.IP(), PORT), shell=True).wait()
